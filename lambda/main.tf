eb4311926b88855d9dae1296bf80429e4335b514
Rob Bayliss <rob@lastcallmedia.com>
2018-10-23 08:42:25 -0400

    Add a lambda module

---
+++ lambda/main.tf
@@ -0,0 +1,75 @@
+
+resource "aws_lambda_function" "default" {
+  function_name = "${var.name}"
+  filename = "${var.package}"
+  handler = "${var.handler}"
+  role = "${aws_iam_role.default.arn}"
+  source_code_hash = "${base64sha256(file("${var.package}"))}"
+  runtime = "${var.runtime}"
+  timeout = "${var.timeout}"
+  vpc_config {
+    security_group_ids = ["${var.security_groups}"]
+    subnet_ids = ["${var.subnets}"]
+  }
+  environment {
+    variables = "${var.environment_variables}"
+  }
+  tags = "${merge(var.tags, map(
+      "Name", "${var.name}"
+  ))}"
+}
+
+
+/**
+ * Roles/Permissioning
+ */
+data "aws_iam_policy_document" "assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "default" {
+  name = "${var.name}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume.json}"
+}
+resource "aws_iam_role_policy_attachment" "access_vpc" {
+  role = "${aws_iam_role.default.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+resource "aws_iam_role_policy_attachment" "additional_policies" {
+  count = "${length(var.iam_policy_arns)}"
+  policy_arn = "${element(var.iam_policy_arns, count.index)}"
+  role = "${aws_iam_role.default.name}"
+}
+resource "aws_iam_role_policy" "additional_policies" {
+  count = "${length(var.iam_policies)}"
+  policy = "${element(var.iam_policies, count.index)}"
+  role = "${aws_iam_role.default.id}"
+}
+
+/**
+ * Scheduling
+ */
+resource "aws_cloudwatch_event_rule" "schedule" {
+  count = "${length(var.schedule)}"
+  name = "${var.name}-${element(keys(var.schedule), count.index)}"
+  schedule_expression = "${element(values(var.schedule), count.index)}"
+}
+resource "aws_cloudwatch_event_target" "schedule_target" {
+  count = "${aws_cloudwatch_event_rule.schedule.count}"
+  arn = "${aws_lambda_function.default.arn}"
+  rule = "${element(aws_cloudwatch_event_rule.schedule.*.name, count.index)}"
+  target_id = "${var.name}_${count.index}"
+}
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+  count = "${aws_cloudwatch_event_rule.schedule.count}"
+  statement_id = "${var.name}-${element(keys(var.schedule), count.index)}"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.default.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${element(aws_cloudwatch_event_rule.schedule.*.arn, count.index)}"
+}
---
+++ lambda/variables.tf
@@ -0,0 +1,49 @@
+
+variable "name" {
+  type = "string"
+}
+variable "package" {
+  type = "string"
+}
+variable "handler" {
+  type = "string"
+  default = "index.handler"
+}
+variable "runtime" {
+  type = "string"
+}
+variable "timeout" {
+  type = "string"
+  default = 300
+}
+variable "security_groups" {
+  type = "list"
+  default = []
+}
+variable "subnets" {
+  type = "list"
+  default = []
+}
+variable "environment_variables" {
+  type = "map"
+  default = {}
+}
+variable "iam_policy_arns" {
+  type = "list"
+  description = "A list of additional IAM policy ARNs to attach to the function's role."
+  default = []
+}
+variable "iam_policies" {
+  type = "list"
+  description = "A list of additional IAM policies to attach to the function."
+  default = []
+}
+variable "schedule" {
+  type = "map"
+  description = "Schedule expressions to use to invoke the lambda regularly"
+  default = {}
+}
+variable "tags" {
+  type = "map"
+  default = {}
+}
