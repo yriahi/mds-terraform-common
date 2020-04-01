797d1e11523594bd71c8e302a42c5d3fd778cc3f
Youssef Riahi <youssef.riahi@mass.gov>
2020-03-31 22:27:44 -0400

    remove webhooks + renaming

--- ci-codebuild/ci/main.tf
+++ ci-codebuild/ci/main.tf
@@ -8,67 +8,9 @@ locals {
   secrets_namespace = "tf/${var.namespace}"
 }
 
-resource "aws_codebuild_project" "plan" {
-  name = "${var.name}-plan"
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE", "LOCAL_CUSTOM_CACHE"]
-  }
-  environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image        = var.build_image
-    type         = "LINUX_CONTAINER"
 
-    # Use privileged mode to allow Docker images to be built.
-    privileged_mode = true
-    environment_variable {
-      name  = "CHAMBER_KMS_KEY_ALIAS"
-      value = var.chamber_key
-    }
-    environment_variable {
-      name  = "CHAMBER_NAMESPACE"
-      value = local.secrets_namespace
-    }
-  }
-  service_role = aws_iam_role.plan.arn
-  source {
-    type                = "CODEBUILD"
-    location            = var.repository
-    git_clone_depth     = "1"
-    report_build_status = true
-    buildspec           = ".pipeline/plan.yml"
-  }
-  tags = merge(
-    var.tags,
-    {
-      "Name" = "${var.name}-plan"
-    },
-  )
-}
-
-resource "aws_codebuild_webhook" "plan" {
-  project_name  = aws_codebuild_project.plan.name
-  branch_filter = "^(?!master|develop).*$"
-}
-
-resource "aws_codebuild_webhook" "apply_develop" {
-  project_name  = aws_codebuild_project.apply_develop.name
-  branch_filter = "^develop$"
-
-  // We use a provisioner here to remove the pull_request event from the apply
-  // webhook.  Otherwise, this codebuild job will run for every PR.
-  // We use a provisioner here to remove the pull_request event from the apply
-  // webhook.  Otherwise, this codebuild job will run for every PR.
-  provisioner "local-exec" {
-    command = "${path.module}/src/disable-webhook-pr.sh \"${aws_codebuild_webhook.apply_develop.url}\" \"${var.oauth_token}\""
-  }
-}
-
-resource "aws_codebuild_project" "apply_develop" {
-  name = "${var.name}-applydev"
+resource "aws_codebuild_project" "test_develop" {
+  name = "${var.name}-testdev"
   artifacts {
     type = "NO_ARTIFACTS"
   }
@@ -102,7 +44,7 @@ resource "aws_codebuild_project" "apply_develop" {
     location            = var.repository
     git_clone_depth     = "1"
     report_build_status = true
-    buildspec           = ".pipeline/apply.yml"
+    buildspec           = ".codebuild/test.yml"
   }
   tags = merge(
     var.tags,
@@ -112,20 +54,7 @@ resource "aws_codebuild_project" "apply_develop" {
   )
 }
 
-resource "aws_codebuild_webhook" "apply_master" {
-  project_name  = aws_codebuild_project.apply_master.name
-  branch_filter = "^master$"
-
-  // We use a provisioner here to remove the pull_request event from the apply
-  // webhook.  Otherwise, this codebuild job will run for every PR.
-  // We use a provisioner here to remove the pull_request event from the apply
-  // webhook.  Otherwise, this codebuild job will run for every PR.
-  provisioner "local-exec" {
-    command = "${path.module}/src/disable-webhook-pr.sh \"${aws_codebuild_webhook.apply_master.url}\" \"${var.oauth_token}\""
-  }
-}
-
-resource "aws_codebuild_project" "apply_master" {
+resource "aws_codebuild_project" "deploy_master" {
   name = "${var.name}-applymas"
   artifacts {
     type = "NO_ARTIFACTS"
@@ -160,7 +89,7 @@ resource "aws_codebuild_project" "apply_master" {
     location            = var.repository
     git_clone_depth     = "1"
     report_build_status = true
-    buildspec           = ".pipeline/apply.yml"
+    buildspec           = ".codebuild/deploy.yml"
   }
   tags = merge(
     var.tags,
